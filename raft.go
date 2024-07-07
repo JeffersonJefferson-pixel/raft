d4c7f468d7dc748215ca71af59d448b1b2af8755
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2024-07-07 14:18:15 +0700

    init election

---
+++ raft.go
@@ -0,0 +1,226 @@
+package raft
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+)
+
+const DebugCM = 1
+
+type LogEntry struct {
+	Command interface{}
+	Term    int
+}
+
+type CMState int
+
+const (
+	Follower CMState = iota
+	Candidate
+	Leader
+	Dead
+)
+
+type ConsensusModule struct {
+	// mu protects cocurrent access to a CM.
+	mu sync.Mutex
+	// server ID of this CM
+	id int
+
+	// IDs of peers in the cluster.
+	peerIds []int
+
+	// server containing this CM.
+	server *Server
+
+	// Persistent Raft state
+	currentTerm int
+	votedFor    int
+	log         []LogEntry
+
+	// Volatile Raft state
+	state              CMState
+	electionResetEvent time.Time
+}
+
+type RequestVoteArgs struct {
+	Term        int
+	CandidateId int
+}
+
+type RequestVoteReply struct {
+	Term int
+}
+
+type AppendEntriesArgs struct {
+	Term     int
+	LeaderId int
+}
+
+type AppendEntriesReply struct {
+	Term int
+}
+
+func (cm *ConsensusModule) runElectionTimer() {
+	timeoutDuration := cm.electionTimeout()
+	cm.mu.Lock()
+	termStarted := cm.currentTerm
+	cm.mu.Unlock()
+	cm.dlog("election timer started (%v), term=%d", timeoutDuration, termStarted)
+
+	// this loops until either
+	// - disover the election timer is no longer needed, or
+	// - the election timer expiers and this CM becomes a candidate
+	ticker := time.NewTicker(10 * time.Millisecond)
+	defer ticker.Stop()
+	for {
+		<-ticker.C
+		cm.mu.Lock()
+		if cm.state != Candidate && cm.state != Follower {
+			cm.dlog("in election timer state=%s, bailing out", cm.state)
+			cm.mu.Unlock()
+			return
+		}
+
+		if termStarted != cm.currentTerm {
+			cm.dlog("in election timer term changed from %d to %d, bailing out", termStarted, cm.currentTerm)
+			cm.mu.Unlock()
+			return
+		}
+
+		// Start an election if we haven't heard from a leader or haven't voted for someone for the duration of the timeout
+		if elapsed := time.Since(cm.electionResetEvent); elapsed >= timeoutDuration {
+			cm.startElection()
+			cm.mu.Unlock()
+			return
+		}
+		cm.mu.Unlock()
+	}
+}
+
+func (cm *ConsensusModule) startElection() {
+	cm.state = Candidate
+	cm.currentTerm += 1
+	savedCurrentTerm := cm.currentTerm
+	cm.electionResetEvent = time.Now()
+	cm.votedFor = cm.id
+	cm.dlog("becomes Candidate (currentTerm=%d); log=%v", savedCurrentTerm, cm.log)
+
+	votesReceived := 1
+
+	// Send RequestVotes RPCs to all other servers concurrently
+	for _, peerId := range cm.peerIds {
+		go func(peerid int) {
+			args := RequestVoteArgs{
+				Term:        savedCurrentTerm,
+				CandidateId: cm.id,
+			}
+			var reply RequestVoteReply
+
+			cm.dlog("sending RequestVote to %d: %+v", peerId, args)
+			if err := cm.server.Call(peerId, "ConsensusModule.RequestVote", args, &reply); err == nil {
+				cm.mu.Lock()
+				defer cm.mu.Unlock()
+				cm.dlog("received RequestVoteReply %+v", reply)
+
+				if cm.state != Candidate {
+					cm.dlog("while waiting for reply, state = %v", cm.state)
+				}
+
+				if reply.Term > savedCurrentTerm {
+					cm.dlog("term out of date in RequestVoteReply")
+					cm.becomeFollower(reply.Term)
+					return
+				} else if reply.Term == savedCurrentTerm {
+					votesReceived++
+					if votesReceived*2 > len(cm.peerIds)+1 {
+						// Won the election
+						cm.dlog("wins election with %d votes", votesReceived)
+						cm.startLeader()
+						return
+					}
+				}
+			}
+		}(peerId)
+	}
+}
+
+func (cm *ConsensusModule) startLeader() {
+	cm.state = Leader
+	cm.dlog("becomes Leader; term=%d, log=%v", cm.currentTerm, cm.log)
+
+	go func() {
+		ticker := time.NewTicker(50 * time.Millisecond)
+		defer ticker.Stop()
+
+		// Send periodic heartbeats, as long as still leader.
+		for {
+			cm.leaderSendHeartbeats()
+			<-ticker.C
+
+			cm.mu.Lock()
+			if cm.state != Leader {
+				cm.mu.Unlock()
+				return
+			}
+			cm.mu.Unlock()
+		}
+	}()
+}
+
+func (cm *ConsensusModule) leaderSendHeartbeats() {
+	cm.mu.Lock()
+	savedCurrentTerm := cm.currentTerm
+	cm.mu.Unlock()
+
+	for _, peerId := range cm.peerIds {
+		args := AppendEntriesArgs{
+			Term:     savedCurrentTerm,
+			LeaderId: cm.id,
+		}
+		go func(peerId int) {
+			cm.dlog("sending AppendEntries to %v: ni=%d, args=%+v", peerId, 0, args)
+			var reply AppendEntriesReply
+			if err := cm.server.Call(peerId, "ConsensusModule.AppendEntries", args, &reply); err == nil {
+				cm.mu.Lock()
+				defer cm.mu.Unlock()
+				if reply.Term > savedCurrentTerm {
+					cm.dlog("term out of date in heartbeat reply")
+					cm.becomeFollower(reply.Term)
+					return
+				}
+			}
+		}(peerId)
+	}
+}
+
+func (cm *ConsensusModule) becomeFollower(term int) {
+	cm.dlog("becomes Follower with term=%d; log=%v", term, cm.log)
+	cm.state = Follower
+	cm.currentTerm = term
+	cm.votedFor = -1
+	cm.electionResetEvent = time.Now()
+
+	go cm.runElectionTimer()
+}
+
+// generates a pseudo-random election timeout duration.
+func (cm *ConsensusModule) electionTimeout() time.Duration {
+	if len(os.Getenv("RAFT_FORCE_MORE_REELECTION")) > 0 && rand.Intn(3) == 0 {
+		return time.Duration(150) * time.Microsecond
+	} else {
+		return time.Duration(150+rand.Intn(150)) * time.Millisecond
+	}
+}
+
+// logs a dubug message if DebugCM > 0.
+func (cm *ConsensusModule) dlog(format string, args ...interface{}) {
+	if DebugCM > 0 {
+		format = fmt.Sprintf("[%d] ", cm.id) + format
+		log.Printf(format, args...)
+	}
+}
---
+++ server.go
@@ -0,0 +1,25 @@
+package raft
+
+import (
+	"fmt"
+	"net/rpc"
+	"sync"
+)
+
+type Server struct {
+	mu sync.Mutex
+
+	peerClients map[int]*rpc.Client
+}
+
+func (s *Server) Call(id int, serviceMethod string, args interface{}, reply interface{}) error {
+	s.mu.Lock()
+	peer := s.peerClients[id]
+	s.mu.Unlock()
+
+	if peer == nil {
+		return fmt.Errorf("call client %d after it's closed", id)
+	} else {
+		return peer.Call(serviceMethod, args, reply)
+	}
+}
